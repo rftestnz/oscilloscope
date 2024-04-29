ad61d72f31b9f2c5aa08f6f864826c0d94b0fa0a
dave.kelly <dave.kelly@rftest.co.nz>
2024-04-30 11:31:09 +1200

    create a simple layout with checkboxes for each test name

--- individual_test_selector.py
+++ individual_test_selector.py
@@ -19,10 +19,36 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QStatusBar,
     QDialog,
+    QVBoxLayout,
+    QDialogButtonBox,
 )
 
 
-class IndividualTestSelector(QDialog, object):
+class IndividualTestSelector(QDialog):
 
     def __init__(self, test_names: list):
-        super.__init__()
+        super().__init__()
+
+        self.selector = QDialog()
+        self.selector.setWindowTitle("Select tests to perform")
+
+        self.layout1 = QVBoxLayout()
+
+        self.layout1.addWidget(QLabel("Select tests to perform"))
+
+        for name in test_names:
+            cb = QCheckBox(name)
+            self.layout1.addWidget(cb)
+
+        buttons = (
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonBox = QDialogButtonBox(buttons)
+
+        self.layout1.addWidget(self.buttonBox)
+
+        self.selector.setLayout(self.layout1)
+
+    def show(self) -> None:
+
+        self.selector.exec()
