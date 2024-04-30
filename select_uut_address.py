9faf26ad679539c5a59b1e13166a94cfdbc9df22
dave.kelly <dave.kelly@rftest.co.nz>
2024-05-01 06:54:40 +1200

    class to show attached USB devices for selection

---
+++ select_uut_address.py
@@ -0,0 +1,76 @@
+"""
+ Dialog to show all of the USB addresses of attached devices
+"""
+
+from PyQt6 import uic
+from PyQt6.QtCore import QObject, QSettings, Qt
+from PyQt6.QtGui import QAction, QIcon, QPixmap
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QRadioButton,
+    QComboBox,
+    QFileDialog,
+    QGroupBox,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMenuBar,
+    QMessageBox,
+    QPushButton,
+    QStatusBar,
+    QDialog,
+    QVBoxLayout,
+    QDialogButtonBox,
+    QWidget,
+)
+
+
+class AddressSelector(QDialog):
+
+    def __init__(self, address_list: list) -> None:
+        super().__init__()
+
+        self.radio_buttons: list[QRadioButton] = []
+        self.uut_address = ""
+
+        self.selector = QDialog()
+
+        self.selector.setWindowTitle("Select UUT address")
+
+        self.layout1 = QVBoxLayout()
+
+        self.layout1.addWidget(QLabel("Select UUT Address"))
+
+        for addr in address_list:
+            rb = QRadioButton(addr)
+            self.radio_buttons.append(rb)
+            self.layout1.addWidget(rb)
+
+        buttons = (
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonBox = QDialogButtonBox(buttons)
+
+        self.buttonBox.clicked.connect(self.button_pressed)
+
+        self.layout1.addWidget(self.buttonBox)
+
+        self.selector.setLayout(self.layout1)
+
+    def show(self) -> None:
+
+        self.selector.exec()
+
+    def button_pressed(self, button: QPushButton) -> None:
+
+        self.uut_address = []
+
+        if button.text() == "OK":
+            # Get the selected radio button
+            for rb in self.radio_buttons:
+                if rb.isChecked():
+                    self.uut_address = rb.text()
+                    break
+
+        self.selector.close()
